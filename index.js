fb206ce8795dddf6131c3b10eab681924f87cb23
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-17 09:25:02 -0600

    commit tree 68f82431d55a98c38dbef6467d0d9da0110eb086

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = (num1 * num2)
+
+let random = Math.round((Math.random() + 1))
+
+const num3 = 6
+const num4 = 2
+
+const mod = (num3 - num4)
+
+const max = Math.max(1, 20)
